61f6ed4af6ca867e911370a382e55e1788e2ead9
Preethi <preethisreejit@gmail.com>
2025-10-13 12:57:14 +0530

    update socket url

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:3000", { withCredentials: true });
+const socket: Socket = io(`import.meta.env.VITE_REACT_APP_SOCKET_URL`, { withCredentials: true });
 
 export default socket;
--- frontend/src/vite.config.ts
+++ frontend/src/vite.config.ts
@@ -4,9 +4,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Replace these with Docker build args injected via process.env
+    'import.meta.env.VITE_REACT_APP_API_BASE_URL': JSON.stringify(process.env.FRONTEND_VITE_REACT_APP_API_BASE_URL),
+    'import.meta.env.VITE_REACT_APP_SOCKET_URL': JSON.stringify(process.env.FRONTEND_VITE_REACT_APP_SOCKET_URL),
+  },
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/vitest.setup.ts", // 👈 important
+    setupFiles: "./src/vitest.setup.ts", 
   },
 });
