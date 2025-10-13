509a8e11ad25580a8fc897d533a9c95d65244d27
Preethi <preethisreejit@gmail.com>
2025-10-13 13:42:58 +0530

    remove hyphon

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io(`import.meta.env.VITE_REACT_APP_SOCKET_URL`, { withCredentials: true });
+const socket: Socket = io(import.meta.env.VITE_REACT_APP_SOCKET_URL, { withCredentials: true });
 
 export default socket;
